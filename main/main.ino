48abc78e113440f0fe69251a5e6fc6a6d6be3f51
ConnorNoddin <connor.noddin@maine.edu>
2023-09-13 09:55:13 -0400

    Added sketch files

---
+++ main/main.ino
@@ -0,0 +1,19 @@
+// GPIO pin assignments for mouse buttons
+const int left = 2;  // Left mouse button
+const int middle = 13;    // Middle mouse button
+const int right = 4; // Right mouse button
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
