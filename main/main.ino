cbc3b354f744ef6dea1de1a932cdde075c5898e3
ConnorNoddin <connor.noddin@maine.edu>
2023-09-14 09:56:01 -0400

    Adding code for mouse buttons

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,33 @@
 // GPIO pin assignments for mouse buttons
-const int left = 2;  // Left mouse button
-const int middle = 13;    // Middle mouse button
-const int right = 4; // Right mouse button
+const int left = 5;  // Left mouse button
+const int middle = 6;    // Middle mouse button
+const int right = 7; // Right mouse button
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  // initialize the mouse buttons as inputs:
+  pinMode(left, INPUT);
+  pinMode(middle, INPUT);
+  pinMode(right, INPUT);
 
-
-  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // read the state of the pushbutton value:
+  leftState = digitalRead(left);
+  middleState = digitalRead(middle);
+  rightState = digitalRead(right);
+
+    // Check the value of the left mouse button
+  if (leftState == HIGH) {
+    // Debugging
+    Serial.print("Left is high!");
+  } else {
+    // Debugging
+    Serial.print("Left is low!");
+  }
+
+
+  sleep(100)
 }
