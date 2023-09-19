1832e36e2a86e9a2fc123d932c2ae252ced2c467
ConnorNoddin <connor.noddin@maine.edu>
2023-09-19 13:32:23 -0400

    Comments

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,22 @@
+// Connor Noddin
+// ECE 406
+// Computer Engineering Capstone
+
 // GPIO pin assignments for mouse buttons
 const int left = 5;  // Left mouse button
 const int middle = 6;    // Middle mouse button
 const int right = 7; // Right mouse button
 
+int leftState, middleState, rightState; // If button is pressed or not
+
 void setup() {
-  // initialize the mouse buttons as inputs:
+  // Initialize the mouse buttons as inputs:
   pinMode(left, INPUT);
   pinMode(middle, INPUT);
   pinMode(right, INPUT);
+
+  // Built in LED for debugging
+  pinMode(LED_BUILTIN, OUTPUT);
 
 }
 
@@ -19,15 +28,17 @@ void loop() {
   middleState = digitalRead(middle);
   rightState = digitalRead(right);
 
-    // Check the value of the left mouse button
+  // Check the value of the left mouse button
   if (leftState == HIGH) {
     // Debugging
     Serial.print("Left is high!");
+    digitalWrite(LED_BUILTIN, HIGH);
+
   } else {
     // Debugging
     Serial.print("Left is low!");
+    digitalWrite(LED_BUILTIN, LOW);
   }
 
-
-  sleep(100)
+  delay(100); // Delay measured in ms
 }
