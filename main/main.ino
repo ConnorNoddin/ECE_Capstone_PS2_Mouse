02fd7d8b566ff77a6692174a04cc299efaebfad6
ConnorNoddin <connor.noddin@maine.edu>
2023-09-20 12:00:20 -0400

    Added PS/2 protocol functions

--- main/main.ino
+++ main/main.ino
@@ -2,16 +2,29 @@
 // ECE 406
 // Computer Engineering Capstone
 
+// Packet Manipulation
 #define R_BUTTON 0x02
 #define L_BUTTON 0x01
 #define M_BUTTON 0x04
 
+// PS/2 Commands
 #define RESET 0xff
+#define BAT 0xAA
+#define ID 0x00
+#define ENABLE 0xF4
+#define GET_DEV_ID 0xF2
+#define STATUS_REQ 0xE9
+#define ACK 0xFA
+
 
 // GPIO pin assignments for mouse buttons
 const int left = 2;  // Left mouse button
 const int middle = 3;    // Middle mouse button
 const int right = 4; // Right mouse button
+
+// GPIO pin assignments for PS/2 connection
+const int DATA = 5;
+const int CLK = 6;
 
 int leftState, middleState, rightState; // If button is pressed or not
 
@@ -30,7 +43,10 @@ void setup() {
   pinMode(middle, INPUT);
   pinMode(right, INPUT);
 
-  byte_1 = byte_1 & 0x08; // Bit 3 is always 1
+  pinMode(DATA, OUTPUT);
+  pinMode(CLK, OUTPUT);
+
+  byte_1 = byte_1 | 0x08; // Bit 3 is always 1
 
 }
 
@@ -42,32 +58,96 @@ void loop() {
   middleState = digitalRead(middle);
   rightState = digitalRead(right);
 
+  //Default state of switches is high
   // Check the value of the left mouse button
-  if (leftState == HIGH) {
-    // Debugging
-    Serial.print("Left is high!\n");
+  if (leftState == LOW) {
+    byte_1 = byte_1 | L_BUTTON;
   } else {
-    // Debugging
-    Serial.print("Left is low!\n");
+    byte_1 = byte_1 & ~L_BUTTON;
   }
-
     // Check the value of the right mouse button
-  if (rightState == HIGH) {
-    // Debugging
-    Serial.print("Right is high!\n");
+  if (rightState == LOW) {
+    byte_1 = byte_1 | R_BUTTON;
   } else {
-    // Debugging
-    Serial.print("Right is low!\n");
+    byte_1 = byte_1 & ~R_BUTTON;
+  }
+  // Check the value of the middle mouse button
+  if (middleState == LOW) {
+    byte_1 = byte_1 | M_BUTTON;
+
+  } else {
+    byte_1 = byte_1 & ~M_BUTTON;
   }
 
-    // Check the value of the middle mouse button
-  if (middleState == HIGH) {
-    // Debugging
-    Serial.print("Middle is high!\n");
-  } else {
-    // Debugging
-    Serial.print("Middle is low!\n");
-  }
+  //Debugging
+  Serial.print("Byte 1: 0x");
+  Serial.print(byte_1, HEX);
+  Serial.print("\n");
 
-  delay(1000); // Delay measured in ms
+  delay(10); // Delay measured in ms
 }
+
+/* Sets the clock to 1, then back to 0 after a delay 
+ The clock frequency is 10-16.7 kHz.  The time from the rising edge of a clock pulse to a Data transition must be at least 5 microseconds. 
+ The time from a data transition to the falling edge of a clock pulse must be at least 5 microseconds and no greater than 25 microseconds. 
+*/
+int ps2_clock(void)
+{
+  //Sets clock to high
+	digitalWrite(CLK, HIGH);
+  delayMicroseconds(15);
+	digitalWrite(CLK, LOW);
+  //delayMicroseconds(15);
+	return 0;
+}
+
+/* Writes data to the DATA PS/2 line */
+int ps2_dwrite(byte ps2_Data)
+{
+
+  int p; // Parity check
+
+  // First bit is always 0
+  digitalWrite(DATA, LOW);
+  ps2_clock();
+
+  p = parity(ps2_Data); //Gets parity before bit shift
+
+  //Send entire byte, LSB first
+  for (int i = 0; i < 8; i++) {
+    if ((ps2_Data & 0x01) == 0x01) digitalWrite(DATA, HIGH); //Writes high if least significant bit is 0
+    else digitalWrite(DATA, LOW); //Writes low if least significant bit is 0
+    ps2_clock(); //Clocks current data
+    ps2_Data = ps2_Data >> 1; //Get next bit
+  }
+
+  // Check parity
+  if (parity == 1) {
+    digitalWrite(DATA, LOW); //Low if odd number of ones
+    ps2_clock();
+  } else {
+    digitalWrite(DATA, HIGH); // High if even number of ones
+    ps2_clock();
+  }
+
+  // Stop bit is always 1
+  digitalWrite(DATA, HIGH); // Always high
+  ps2_clock();
+}
+
+// Check parity of byte
+int parity(byte p_check) 
+{ 
+  byte ones = 0; //Total number of ones
+
+  for (int i = 0; i < 8; i++) {
+    if ((p_check & 0x01) == 0x01) {
+      ones++;
+    }
+    p_check = p_check >> 1;
+}
+ 
+
+  return (ones & 0x01); //Checks if parity is odd
+}
+
